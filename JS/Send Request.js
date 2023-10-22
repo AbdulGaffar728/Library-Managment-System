c0de09d9dc268f99f975dd5e44653ee57ede9be9
AbdulGaffar728 <abdulgaffar4895728@gmail.com>
2023-10-22 20:01:05 +0100

    Add send request page

---
+++ JS/Send Request.js
@@ -0,0 +1,25 @@
+var Requests;
+sendingRequest=()=>{
+var RequestsInStorage=JSON.parse(localStorage.getItem("Requests"))
+if(RequestsInStorage===null){
+    Requests=[];
+}
+else{
+    Requests=RequestsInStorage;
+}
+ if(document.getElementById("numberOfDays").value<=7){
+var Request={
+    userName:document.getElementById("userName").value,
+    userEmai:document.getElementById("userEmail").value,
+    bookName:document.getElementById("bookName").value,
+    numberOfDays:document.getElementById("numberOfDays").value
+}
+Requests.push(Request);
+localStorage.setItem("Requests",JSON.stringify(Requests))
+alert("Your request is submitted.");
+// window.location.href="User Sitssssssse.html";
+    }
+    else{
+        alert("Plz enter maximum 7 days.")
+    }
+}
